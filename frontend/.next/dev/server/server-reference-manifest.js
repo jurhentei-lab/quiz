f67e18942af0bba3756e71178c62c7003dc75b68
25LP5459 25LP5459 <25LP5459@G302-CC0111.local>
2026-03-10 21:18:09 +0800

    done

--- frontend/.next/dev/server/server-reference-manifest.js
+++ frontend/.next/dev/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"g9kefcHRtOD6RgG37tmebFM+W+ZlrN8KX8KID1k+2NE=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
--- frontend/src/app/components/Header.tsx
+++ frontend/src/app/components/Header.tsx
@@ -2,10 +2,6 @@ const Header = () => {
   return (
     <div className="w-full h-14 flex justify-between px-6 py-2 border border-gray-200">
       <p className="text-2xl font-semibold font-[GIP]">AI Quiz Generator</p>
-      <div
-        className="w-10 h-10 bg-center bg-cover rounded-full"
-        style={{ backgroundImage: "url('/lubu.jpeg')" }}
-      ></div>
     </div>
   );
 };
